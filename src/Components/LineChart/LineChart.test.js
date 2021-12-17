244adc76a7c43e5419481bf2ac9233bf62bd6bcc
Ozan İncesulu <ozyinc@users.noreply.github.com>
2021-12-17 13:08:30 +0100

    test: fix test data

--- src/Components/LineChart/LineChart.test.js
+++ src/Components/LineChart/LineChart.test.js
@@ -2,6 +2,16 @@ import LineChart from "./LineChart";
 import renderer from "react-test-renderer";
 
 test("matches LineChart snapshot", () => {
-  const tree = renderer.create(<LineChart />).toJSON();
-  expect(tree).toMatchSnapshot();
+    const data = [
+        {
+            valueOne: "test", //X-axis
+            valueTwo: 20 //Y-axis
+        },
+        {
+            valueOne: "test2",
+            valueTwo: 30
+        }
+    ]
+    const tree = renderer.create(<LineChart data={data}/>).toJSON();
+    expect(tree).toMatchSnapshot();
 });
