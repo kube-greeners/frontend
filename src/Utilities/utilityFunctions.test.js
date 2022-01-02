55589c64a0226a13460359e25c3a179d53596eb0
Hanna <hanntorj@stud.ntnu.no>
2022-01-02 16:36:41 +0100

    shortened testdata and fixed localtimeissue

--- src/Utilities/utilityFunctions.test.js
+++ src/Utilities/utilityFunctions.test.js
@@ -1,12 +1,21 @@
-import { mockedResData, convertedData, extractedNamespaces } from "./MockedResData";
+import {
+  mockedResData,
+  convertedData,
+  extractedNamespaces,
+} from "./MockedResData";
 
 const {
+  convertDate,
   convertData,
   extractNamespaceName,
   createQueryparams,
 } = require("./utilityFunctions");
 
 const data = mockedResData;
+
+for (const d of convertedData.historicalData) {
+  d["Date"] = convertDate(d["Date"]);
+}
 
 test("Properly converts data", () => {
   expect(convertData(data, "Memory Usage")).toEqual(convertedData);
@@ -17,7 +26,10 @@ test("Properly extractes the namespace names", () => {
 });
 
 test("Properly creates query params", () => {
-  expect(createQueryparams("frontend", 1639916278206, 1641125802316)).toStrictEqual(`?namespace=frontend&start=1639916278206&end=1641125802316`);
-  expect(createQueryparams(null, 1639916278206, 1641125802316)).toStrictEqual(`?start=1639916278206&end=1641125802316`);
-
-})
+  expect(
+    createQueryparams("frontend", 1639916278206, 1641125802316)
+  ).toStrictEqual(`?namespace=frontend&start=1639916278206&end=1641125802316`);
+  expect(createQueryparams(null, 1639916278206, 1641125802316)).toStrictEqual(
+    `?start=1639916278206&end=1641125802316`
+  );
+});
