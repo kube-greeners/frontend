4e1e56439f179f46cf13a9f73fb7d28831604910
Boris Grunwald <borisgrunwald@gmail.com>
2022-01-06 20:38:00 +0100

    Show "something went wrong" on server failure

--- src/Components/StatComponent/StatComponent.js
+++ src/Components/StatComponent/StatComponent.js
@@ -21,16 +21,20 @@ function StatComponent({
   
  const decimals = integer? 0 : 2;
 
+ const content = !isError ? (
+   success2
+     ? success1 && success2
+       ? (stat1 && stat2) ? `${stat1.toFixed(decimals)} ${unit} / ${stat2.toFixed(decimals)} ${unit}` : "No data"
+       : "Loading..."
+     : success1
+     ? stat1 ? `${stat1.toFixed(decimals)} ${unit ? unit : ""}` : "No data"
+     : "Loading..."
+ ) : "Something went wrong."
+
   return (
     <>
       <Card style={{ ...statContainerStyle, gridArea: gridArea }} title={title}>
-        {success2
-          ? success1 && success2
-            ? (stat1 && stat2) ? `${stat1.toFixed(decimals)} ${unit} / ${stat2.toFixed(decimals)} ${unit}` : "No data"
-            : "Loading..."
-          : success1
-          ? stat1 ? `${stat1.toFixed(decimals)} ${unit ? unit : ""}` : "No data"
-          : "Loading..."}
+        {content}
       </Card>
     </>
   );
