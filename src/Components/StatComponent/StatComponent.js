fc5312e74927878c4f62a7b68c0a108c84f1373c
Ozan İncesulu <ozyinc@users.noreply.github.com>
2022-01-07 07:26:13 +0100

    bugfix: the data is shown when the backend response is 0

--- src/Components/StatComponent/StatComponent.js
+++ src/Components/StatComponent/StatComponent.js
@@ -21,16 +21,15 @@ function StatComponent({
   };
   
  const decimals = integer? 0 : 2;
-
   return (
     <>
       <Card style={{ ...statContainerStyle, gridArea: gridArea }} title={title}>
         {success2
           ? success1 && success2
-            ? (stat1 && stat2) ? `${stat1.toFixed(decimals)} ${unit} / ${stat2.toFixed(decimals)} ${unit}` : "No data"
+            ? (Number.isFinite(stat1) && Number.isFinite(stat2)) ? `${stat1.toFixed(decimals)} ${unit} / ${stat2.toFixed(decimals)} ${unit}` : "No data"
             : "Loading..."
           : success1
-          ? stat1 ? `${stat1.toFixed(decimals)} ${unit ? unit : ""}` : "No data"
+          ? Number.isFinite(stat1)? `${stat1.toFixed(decimals)} ${unit ? unit : ""}` : "No data"
           : "Loading..."}
       </Card>
     </>
