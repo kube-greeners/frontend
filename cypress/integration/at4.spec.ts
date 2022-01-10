a87c6c0332533b7965ac0065b3da93a3187dbe60
Ozan İncesulu <ozyinc@users.noreply.github.com>
2022-01-10 06:51:42 +0100

    Fix a test in at4 and remove the canvas testing one

--- cypress/integration/at4.spec.ts
+++ cypress/integration/at4.spec.ts
@@ -43,7 +43,8 @@ describe("Time interval selector", () => {
         cy.intercept("GET", Cypress.config("baseUrl") + "/co2_emission_with_kube_green?*").as("savedRequest");
 
 
-        cy.get("input[placeholder='End date']").focus().type("04/01/2022").trigger("keydown", {key: 13}).blur();
+        cy.get("input[placeholder='Start date']").focus().clear().type("01/01/2022").trigger("keydown", {key: 13});
+        cy.get("input[placeholder='End date']").focus().clear().type("04/01/2022").trigger("keydown", {key: 13}).blur();
         ready();
         cy.wait("@savedRequest", {timeout: 60000});
         cy.get("canvas", {timeout: 20000}).screenshot("new", {overwrite: true});
@@ -52,24 +53,3 @@ describe("Time interval selector", () => {
         });
     });
 });
-
-describe("Time interval graph slider", () => {
-    before(() => {
-        cy.cacheRequests();
-        cy.visit("/dev/");
-    });
-    it("should readjust the graph when changed", () => {
-        cy.get("canvas", {timeout: 60000});
-        const old_canvas_path = `cypress/screenshots/at4.spec.ts/old.png`;
-        const new_canvas_path = `cypress/screenshots/at4.spec.ts/new.png`;
-        cy.wait(300);
-        cy.get("canvas", {timeout: 20000}).screenshot("old", {overwrite: true});
-
-        cy.get("canvas").trigger("mousedown", 400, 480).trigger("mousemove", 300, 480).trigger("mouseup");
-        cy.wait(500);
-        cy.get("canvas", {timeout: 20000}).screenshot("new", {overwrite: true});
-        cy.task("compareScreenshots", {first: old_canvas_path, second: new_canvas_path}).then(result => {
-            expect(result).to.be.greaterThan(0.1);
-        });
-    })
-})
